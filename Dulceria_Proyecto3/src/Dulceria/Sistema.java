4fc0ac7e8c3c6eb98507a8b33e31ec7949c6b8ed
jhoeg <jhoeg@JHOEWI>
2023-11-17 18:40:17 -0500

    noccc

--- Dulceria_Proyecto3/src/Dulceria/Sistema.java
+++ Dulceria_Proyecto3/src/Dulceria/Sistema.java
@@ -10,7 +10,7 @@ public class Sistema {
 
 		Scanner sc = new Scanner(System.in);
 		Sistema s = new Sistema();
-		Finanza f = new Finanza(); 
+		
 		
 		Tienda tienda = new Tienda("Ciocolatto ","Cra 4 # 8-71", "Ciocolatto@gmail.com", 320456788);
 		
@@ -41,7 +41,7 @@ public class Sistema {
 							 break;
 						 }
 						 case 3:{
-							 s.gananciastienda(f, tienda);
+							 s.gananciastienda( tienda);
 						 }
 						 case 4:{
 							 System.out.println("SESION DE ENCARGADO CERRADA");
@@ -377,7 +377,7 @@ public class Sistema {
 		
 	}
 	
-	public void gananciastienda(Finanza f, Tienda tienda) {
+	public void gananciastienda(Tienda tienda) {
 		Scanner sc = new Scanner(System.in); 
 		
 		int opcion = 0; 
